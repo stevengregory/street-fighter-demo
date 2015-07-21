53c3e56cbc6682373e034562e0b11523b379db81
unknown <sgregory@gmail.com>
2015-07-21 17:42:04 -0500

    Added an immediately invoked arrow function to entrance

--- src/app/entrance.js
+++ src/app/entrance.js
@@ -2,7 +2,7 @@ import { character as c } from './character';
 import { moves as m } from './moves';
 import { keyCode } from './key-code';
 
-var entrance = function() {
+(() => {
     c.addClass('stance');
     m.entrance.execute();
-}();
+})();
