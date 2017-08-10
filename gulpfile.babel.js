85a40cc1319f9e06c90a6d7c2c27e0b1da9791ac
Steven Gregory <sgregory@gmail.com>
2017-08-09 21:50:02 -0500

    Updated & removed old packages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,19 +1,5 @@
 import gulp from 'gulp';
-import htmlhint from 'gulp-htmlhint';
-import jshint from 'gulp-jshint';
 import sass from 'gulp-sass';
-
-gulp.task('htmlhint', () => {
-  return gulp.src('src/index.html')
-    .pipe(htmlhint())
-    .pipe(htmlhint.reporter());
-});
-
-gulp.task('jshint', () => {
-  return gulp.src('src/app/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter());
-});
 
 gulp.task('sass', () => {
   return gulp.src('src/sass/style.scss')
@@ -24,8 +10,6 @@ gulp.task('sass', () => {
 });
 
 gulp.task('watch', () => {
-  gulp.watch('src/index.html', ['htmlhint']);
-  gulp.watch('src/app/*.js', ['jshint']);
   gulp.watch('src/sass/*.scss', ['sass']);
 });
 
