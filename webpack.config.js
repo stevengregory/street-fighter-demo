16354deadfd53ed31c1b323b0e2c472f83ba604d
Steven Gregory <sgregory@gmail.com>
2017-08-24 20:58:25 -0500

    Refactored webpack build & removed gulp

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  context: __dirname + '/src/app',
-  entry: './entrance.js',
+  entry: ['./src/app/entrance.js', './src/sass/style.scss'],
   output: {
-    path: __dirname + '/src',
+    path: __dirname + '/dist',
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './src',
+    contentBase: './dist',
     historyApiFallback: true,
     inline: true,
     open: true,
@@ -21,11 +25,38 @@ module.exports = {
           presets: ['es2015']
         }
       }
+    },
+    {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        use: ['css-loader', 'sass-loader']
+      })
+    },
+    {
+      test: /\.(png|jpg|gif)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[name].[ext]'
+        }
+      }
     }]
   },
   resolve: {
     alias: {
       jquery: 'jquery/src/jquery'
     }
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: 'src/sounds',
+        to: 'sounds'
+      }
+    ])
+  ]
 };
