ae1fc1f893f63e47da1e1f11fb90e83fe990f86e
Steven Gregory <sgregory@gmail.com>
2023-04-10 14:12:03 -0500

    refactor: use native keyup event & remove $

--- src/app/key-code.js
+++ src/app/key-code.js
@@ -1,10 +1,9 @@
-import $ from 'jquery';
 import moves from './moves';
 
-export default (function getKeyCode() {
-  $(document).on('keyup', (e) => {
+export default (function getMove() {
+  onkeyup = (e) => {
     const isMove = (move) => move.key === e.key;
     const getMove = (move) => move.filter(isMove);
-    getMove(moves).length && getMove(moves)[0].doMove();
-  });
+    return getMove(moves).length && getMove(moves)[0].doMove();
+  }
 })();
