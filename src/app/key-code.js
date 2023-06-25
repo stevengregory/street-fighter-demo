2fb9217455fddbdf9879be530288127c31ae3d00
Steven Gregory <sgregory@gmail.com>
2023-06-25 16:11:47 -0500

    refactor: use find over filter in getMove fn

--- src/app/key-code.js
+++ src/app/key-code.js
@@ -3,7 +3,7 @@ import { default as moves } from './moves';
 export default (function getMove() {
   onkeyup = (e) => {
     const isMove = (move) => move.key === e.key;
-    const getMove = (move) => move.filter(isMove);
-    return getMove(moves).length && getMove(moves)[0].doMove();
+    const move = moves.find(isMove);
+    return move && move.doMove();
   };
 })();
