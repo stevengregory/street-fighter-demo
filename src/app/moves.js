9380ead376a7456a709ec621c9811fa4997eb861
Steven Gregory <sgregory@gmail.com>
2025-04-25 12:58:32 -0500

    feat: add corkscrew blow move

--- src/app/moves.js
+++ src/app/moves.js
@@ -4,15 +4,16 @@ export default (function getMoves() {
   return [
     new Action('walk', 'ArrowRight', 30, false),
     new Action('walk-backwards', 'ArrowLeft', -30, false),
-    new Action('jab', 'j', false),
     new Action('cross', 'c', false),
+    new Action('corkscrew-blow', '2', false, false),
     new Action('hook', 'h', false),
-    new Action('uppercut', 'u', false),
+    new Action('jab', 'j', false),
     new Action('jet-uppercut', '1', false),
+    new Action('uppercut', 'u', false),
     new Action('jump', 'ArrowUp', false, false),
     new Action('duck', 'ArrowDown', false, false),
     new Action('block', 'b', false, false),
     new Action('fall', 'f', false),
-    new Action('rest', 'r', false, false)
+    new Action('rest', 'r', false, false),
   ];
 })();
