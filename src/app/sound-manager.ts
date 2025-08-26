ae9151fa9ea0681cb65fc75201d91514e7b9d5e9
Steven Gregory <sgregory@gmail.com>
2025-08-26 11:29:33 -0500

    refactor: replace static class with module-level object

--- src/app/sound-manager.ts
+++ src/app/sound-manager.ts
@@ -1,21 +1,21 @@
 import { Config } from './config';
 
-export class SoundManager {
-  private static musicAudio: HTMLAudioElement | null = null;
+let musicAudio: HTMLAudioElement | null = null;
 
-  static play(soundName: string): void {
+export const SoundManager = {
+  play(soundName: string): void {
     const audio = new Audio(`sounds/${soundName}.mp3`);
     audio.play();
-  }
+  },
 
-  static playMusicTrack(trackName: string): void {
-    if (this.musicAudio) {
-      this.musicAudio.pause();
+  playMusicTrack(trackName: string): void {
+    if (musicAudio) {
+      musicAudio.pause();
     }
     const music = new Audio(`sounds/${trackName}.mp3`);
     music.loop = true;
     music.volume = Config.musicVolume;
-    this.musicAudio = music;
+    musicAudio = music;
     music.play();
-  }
-}
+  },
+};
